5d295105be1965ff7d7bdcc62bf6dc1a09e09d78
Dan A <dan.a.augusti@gmail.com>
2026-05-07 20:53:49 -0400

    ft: fixed verification, added guard functionality, fixed a verification bug in user creation not returning the verification to DB

--- src/auth/roles.guard.ts
+++ src/auth/roles.guard.ts
@@ -1,4 +1,4 @@
-import{
+import {
     Injectable,
     CanActivate,
     ExecutionContext,
@@ -11,28 +11,36 @@ import { ROLES_KEY } from './roles.decorator';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-    constructor(private reflector: Reflector) {}
+    constructor(private reflector: Reflector) { }
 
     canActivate(context: ExecutionContext): boolean {
-    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
-  context.getHandler(), // Checks the specific @Get or @Post method
-  context.getClass(),   // Checks the @Controller class level
-   ]);
-    if (!requiredRoles) return true;
+        const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+            context.getHandler(), // Checks the specific @Get or @Post method
+            context.getClass(),   // Checks the @Controller class level
+        ]);
+        if (!requiredRoles) return true;
 
-    const { user } = context.switchToHttp().getRequest();
+        //find a place to verify 
+        const { user } = context.switchToHttp().getRequest();
 
-    // 🕵️ DEBUG LOGS - Check your NestJS Terminal for these!
-    console.log('--- Roles Guard Debug ---');
-    console.log('Target Route Roles:', requiredRoles);
-    console.log('User Found on Request:', user ? 'YES' : 'NO');
-    console.log('User Role:', user?.role);
+        // 🕵️ DEBUG LOGS - Check your NestJS Terminal for these!
+        console.log('--- Roles Guard Debug ---');
+        console.log('Target Route Roles:', requiredRoles);
+        console.log('User Found on Request:', user ? 'YES' : 'NO');
+        console.log('User Role:', user?.role);
+        //check for verification here as well to prevent token automatically
 
-    if (!user || !requiredRoles.includes(user.role)) {
-        console.error('❌ GUARD REJECTED REQUEST');
-        throw new ForbiddenException('Access Denied');
+        if (user && user.role !== Role.USER && user.verification !== "VERIFIED") {
+
+            console.error('❌ GUARD CAUGHT UNVERIFIED TOKEN');
+            throw new ForbiddenException('Account Must Be Approved By Admin');
+        }
+
+        if (!user || !requiredRoles.includes(user.role)) {
+            console.error('❌ GUARD REJECTED REQUEST');
+            throw new ForbiddenException('Access Denied');
+        }
+
+        return true;
     }
-
-    return true;
-}
 }
